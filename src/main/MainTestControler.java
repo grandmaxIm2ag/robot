08c6e87647e9502c3f6e410f44e6203b832f9f52
Maxence Grand <maxencegrad1995@gmail.com>
2018-03-26 12:43:28 +0200

    Add test controler

---
+++ src/main/MainTestControler.java
@@ -0,0 +1,22 @@
+package main;
+
+import lejos.utility.Delay;
+import controler.TestControler;
+
+public class MainTestControler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestControler controler = new TestControler();
+		try{
+			controler.start();
+		}catch(Throwable e){
+			e.printStackTrace();
+			Delay.msDelay(10000);
+		}
+		System.exit(0);
+	}
+
+}
